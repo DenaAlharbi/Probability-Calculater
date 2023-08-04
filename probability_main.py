98109f3ccb85835fa491209a19944c62f7b8ce89
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-08-04 18:09:06 +0300

    Add files via upload

--- probability_main.py
+++ probability_main.py
@@ -16,6 +16,7 @@ class Object:
     # This is if the user wants to choose just one color
     def oneColor(self, total_balls, numTake, color_num_balls, numCalculate):
         the_calculation1 = scipy.stats.binom.pmf(numCalculate, numTake, color_num_balls / total_balls)
+        print(the_calculation1)
 
     # def balance_update(self):
     # infile = open("record.txt", "w")
@@ -44,37 +45,63 @@ class Inputs:
         self.total_of_balls = None
 
     def specificColorNum(self):
-        one_specific_color = input(
-            "What is the color you want to find the probability of?\n 'red' or 'blue' or 'green':")
-        if one_specific_color == "red":
-            return self.red_user
-        if one_specific_color == "blue":
-            return self.red_user
-        if one_specific_color == "green":
-            return self.green_user
+        global check
+        while not check:
+            one_specific_color = input(
+                "What is the color you want to find the probability of?\n 'red' or 'blue' or 'green':")
+            if one_specific_color == "red":
+                check = True
+                return self.red_user
+            elif one_specific_color == "blue":
+                check = True
+                return self.red_user
+            elif one_specific_color == "green":
+                check = True
+                return self.green_user
+            else:
+                print("Invalid input!")
 
     def numToCalculate(self):
         self.num_balls_col = input("How many balls of that color do you want to take out?")
+        try:
+            self.num_balls_col = int(self.num_balls_col)
+        except ValueError:  # Add something that makes it loop until the user gives the right type
+            self.num_balls_col = input("Invalid input! How many balls of that color do you want to take out?")
         return self.num_balls_col
 
     def red(self):
         self.red_user = input("How many red balls are there in your imaginary hat? ")
-        self.red_user = int(self.red_user)
+        try:
+            self.red_user = int(self.red_user)
+        except ValueError:
+            self.red_user = input("Invalid input! How many red balls are there in your imaginary hat? ")
+
         return self.red_user
 
     def blue(self):
         self.blue_user = input("How many blue balls are there in your imaginary hat? ")
-        self.blue_user = int(self.blue_user)
+        try:
+            self.blue_user = int(self.blue_user)
+        except ValueError:
+            self.blue_user = input("Invalid input! How many blue balls are there in your imaginary hat? ")
+
         return self.blue_user
 
     def green(self):
         self.green_user = input("How many green balls are there in your imaginary hat? ")
-        self.green_user = int(self.green_user)
+        try:
+            self.green_user = int(self.green_user)
+        except ValueError:
+            self.green_user = input("Invalid input! How many green balls are there in your imaginary hat? ")
+
         return self.green_user
 
     def numOfBallsTake(self):  # That the user is going to take out
         self.num_balls_take = input("How many balls do you want to take out? ")
-        self.num_balls_take = int(self.num_balls_take)
+        try:
+            self.num_balls_take = int(self.num_balls_take)
+        except ValueError:
+            self.num_balls_take = input("Invalid input! How many balls do you want to take out? ")
         return self.num_balls_take
 
     def total(self, col_red, col_blue, col_green):
@@ -82,9 +109,14 @@ class Inputs:
         return self.total_of_balls
 
 
+# ---Global Variables ---
+check = False
+# ---
+
+
 def main():
-    objectA = Inputs
-    objectB = Object
+    objectA = Inputs()
+    objectB = Object()
     firstInput = input("Do you want to play the probability game? 'yes' or 'no': ")
     while firstInput != "":
         if Validate(firstInput):
